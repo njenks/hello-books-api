b0fd789280ed295b4b6b0168d13461abc5200875
Natascha <njenkns@gmail.com>
2022-04-17 17:30:34 -0700

    finished hello world routes lesson

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,22 @@ hello_world_bp = Blueprint("hello_world", __name__)
 def say_hello_world():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Natascha",
+        "message": "You got this, guuuurl!", 
+        "hobbies": ["Reading", "Playing Video Games","Hiking", "Traveling"]
+    }
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Natascha",
+        "message": "You got this, guuuurl!", 
+        "hobbies": ["Reading", "Playing Video Games","Hiking", "Traveling"]
+    }
+    new_hobby = ["Eating"]
+    response_body["hobbies"] = response_body["hobbies"] + new_hobby
+    return response_body
