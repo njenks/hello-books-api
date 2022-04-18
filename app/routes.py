7a336dc747c6e27c5bd1a0e61405936c1726e384
Natascha <njenkns@gmail.com>
2022-04-17 17:46:01 -0700

    finished 01 building api lesson

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify 
 
 hello_world_bp = Blueprint("hello_world", __name__)
 
@@ -25,3 +25,28 @@ def broken_endpoint():
     new_hobby = ["Eating"]
     response_body["hobbies"] = response_body["hobbies"] + new_hobby
     return response_body
+
+class Book: 
+    def __init__(self, id, title, description): 
+        self.id = id
+        self.title = title
+        self.description = description
+    
+books = [
+    Book(1, "Pride and Prejudice", "A fantasy novel set in an imaginary world."),
+    Book(2, "Harry Potter", "A fantasy novel set in an imaginary world."),
+    Book(3, "Mexican Gothic", "A fantasy novel set in an imaginary world.")
+]
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id, 
+            "title": book.title, 
+            "description": book.description
+        })
+    return jsonify(books_response)
